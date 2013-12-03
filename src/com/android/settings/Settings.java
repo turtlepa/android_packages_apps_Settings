f30f7ef68a8c465a4b45cd7e18de4dc5eada8e9c
Danny Baumann <dannybaumann@web.de>
2013-12-03 15:15:19 +0100

    Revert "Single pane for 10" Settings"
    This reverts commit 56f51a8927f53bee81145fe29e63af8a87e6fd1b.
    
    Change-Id: If5c7be247caeeb4ec3e0bceed86bc5763d74f927

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -326,11 +326,6 @@ public class Settings extends PreferenceActivity
         }
     }
 
-    @Override
-    public boolean onIsMultiPane() {
-        return false;
-    }
-
     private static final String[] ENTRY_FRAGMENTS = {
         WirelessSettings.class.getName(),
         WifiSettings.class.getName(),
