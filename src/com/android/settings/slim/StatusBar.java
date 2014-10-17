960766544a3158520e535e1b07661489ec8058cb
Jubakuba <Jubakuba@gmail.com>
2014-10-17 00:31:56 -0600

    Settings: StatusBar Ticker (2/2)
    PS: MIspeeliings.
    
    Change-Id: I02cf9192c3ba62111a41e7da52cf4c343a589392

--- src/com/android/settings/slim/StatusBar.java
+++ src/com/android/settings/slim/StatusBar.java
@@ -45,12 +45,14 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private static final String TAG = "StatusBarSettings";
 
     private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
+    private static final String KEY_STATUS_BAR_TICKER = "status_bar_ticker";
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     private static final String STATUS_BAR_DOUBLE_TAP_GESTURE = "status_bar_double_tap_gesture";
 
     private static final String NETWORK_ACTIVITY_ARROWS = "network_activity_arrows";
 
     private PreferenceScreen mClockStyle;
+    private CheckBoxPreference mTicker;
     private CheckBoxPreference mStatusBarBrightnessControl;
     private CheckBoxPreference mStatusBarDoubleTapGesture;
     private CheckBoxPreference mNeworkActivityArrows;
@@ -73,6 +75,11 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             updateClockStyleDescription();
         }
 
+        mTicker = (CheckBoxPreference) prefSet.findPreference(KEY_STATUS_BAR_TICKER);
+        mTicker.setChecked(Settings.System.getInt(
+                getContentResolver(), Settings.System.TICKER_ENABLED, 1) == 1);
+        mTicker.setOnPreferenceChangeListener(this);
+
         mStatusBarBrightnessControl =
             (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_BRIGHTNESS_CONTROL);
         mStatusBarBrightnessControl.setChecked((Settings.System.getInt(getContentResolver(),
@@ -93,7 +100,11 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mStatusBarBrightnessControl) {
+        if (preference == mTicker) {
+            Settings.System.putInt(getContentResolver(), Settings.System.TICKER_ENABLED,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mStatusBarBrightnessControl) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL,
                     (Boolean) newValue ? 1 : 0);
